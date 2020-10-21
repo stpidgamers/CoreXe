16c120b6f7664c5c83264aaab1d69bf973de0d6d
stpidgamers <69773194+stpidgamers@users.noreply.github.com>
2020-10-21 11:06:09 -0400

    plugin.yml Main.java

--- src/me/nitrogen/xecore/Main.java
+++ src/me/nitrogen/xecore/Main.java
@@ -1,4 +1,23 @@
 package me.nitrogen.xecore;
 
-public class Main {
+import me.nitrogen.xecore.Commands.*;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("[!] Enabling CoreXe v1.0.0...");
+        this.getCommand("Gma").setExecutor((CommandExecutor)new GmaCommand());
+        this.getCommand("Fly").setExecutor((CommandExecutor)new FlyCommand());
+        this.getCommand("Gmc").setExecutor((CommandExecutor)new GmcCommand());
+        this.getCommand("Gms").setExecutor((CommandExecutor)new GmsCommand());
+        this.getCommand("Gmsp").setExecutor((CommandExecutor)new GmspCommand());
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("[!] Disabling CoreXe v1.0.0...");
+    }
 }
